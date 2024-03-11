ca237c6fa446227de489f9e2f2656a90388f8647
Dipa Galatian <dipagalatian3003@gmail.com>
2024-03-11 15:06:09 +0700

    create new branch

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -9,7 +9,6 @@ import Lobby from "../screens/Lobby";
 const Stack = createNativeStackNavigator();
 
 export default function Router() {
-<<<<<<< HEAD
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Splash">
             <Stack.Screen name="Splash" component={SplashScreen} />
@@ -20,19 +19,4 @@ export default function Router() {
             {/* <Stack.Screen name="Signin" component={Lobby} /> */}
         </Stack.Navigator>
     );
-=======
-  return (
-    <Stack.Navigator
-      // screenOptions={{ headerShown: false }}
-      initialRouteName="Splash"
-    >
-      <Stack.Screen name="Splash" component={SplashScreen} />
-      <Stack.Screen name="SignIn" component={SignInScreen} />
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Avatar" component={ChooseAvatar} />
-      {/* <Stack.Screen name="Splash" component={Home} /> */}
-      {/* <Stack.Screen name="Signin" component={Lobby} /> */}
-    </Stack.Navigator>
-  );
->>>>>>> c8b52be46410647722493075cb58bef79eb98c95
 }
--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -1,69 +1,41 @@
-import {
-  AddIcon,
-  Button,
-  ButtonIcon,
-  ButtonText,
-  Heading,
-  Image,
-} from "@gluestack-ui/themed";
+import { AddIcon, Button, ButtonIcon, ButtonText, Heading, Image } from "@gluestack-ui/themed";
 import { StyleSheet, ImageBackground, StatusBar } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 const bg1 = require("../../../assets/background/bg1.jpg");
 const logo = require("../../../assets/logo/logo2.png");
-const appName = require("../../../assets/logo/app-name.png");
+const appName = require("../../../assets/logo/gtp3.png");
 
 export default function SplashScreen() {
-  const navigation = useNavigation();
-  return (
-    <ImageBackground source={bg1} style={styles.container}>
-      <Image alt="logo" size="2xl" source={logo} />
-      <Image alt="logo" size="2xl" source={appName} />
-      <Button
-        size="md"
-        variant="solid"
-        action="primary"
-        isDisabled={false}
-        isFocusVisible={false}
-        onPress={() => navigation.navigate("SignIn" as never)}
-      >
-        <ButtonText>login</ButtonText>
-      </Button>
-      <Button
-        size="md"
-        variant="solid"
-        action="primary"
-        isDisabled={false}
-        isFocusVisible={false}
-        onPress={() => navigation.navigate("Home" as never)}
-      >
-        <ButtonText>Home</ButtonText>
-      </Button>
-      <Button
-        size="md"
-        variant="solid"
-        action="primary"
-        isDisabled={false}
-        isFocusVisible={false}
-        onPress={() => navigation.navigate("Avatar" as never)}
-      >
-        <ButtonText>Choose Avatar</ButtonText>
-      </Button>
+    const navigation = useNavigation();
+    return (
+        <ImageBackground source={bg1} style={styles.container}>
+            <Image alt="logo" size="2xl" source={logo} />
+            <Image alt="logo" size="2xl" source={appName} />
+            <Button size="md" variant="solid" action="primary" isDisabled={false} isFocusVisible={false} onPress={() => navigation.navigate("SignIn" as never)}>
+                <ButtonText>login</ButtonText>
+            </Button>
+            <Button size="md" variant="solid" action="primary" isDisabled={false} isFocusVisible={false} onPress={() => navigation.navigate("Home" as never)}>
+                <ButtonText>Home</ButtonText>
+            </Button>
+            <Button size="md" variant="solid" action="primary" isDisabled={false} isFocusVisible={false} onPress={() => navigation.navigate("Avatar" as never)}>
+                <ButtonText>Choose Avatar</ButtonText>
+            </Button>
 
-      <StatusBar hidden />
-    </ImageBackground>
-  );
+            <StatusBar hidden />
+        </ImageBackground>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "lightblue",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontWeight: "bold",
-    fontSize: 50,
-    color: "#3A4D39",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "lightblue",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    title: {
+        fontWeight: "bold",
+        fontSize: 50,
+        color: "#3A4D39",
+    },
 });
